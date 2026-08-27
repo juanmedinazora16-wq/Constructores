5c1fbf1fe2ad5bf94abaf8263a5678dd253c8d18
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:46:56 -0500

    Se creo el metodo constructor de la clase producto

--- producto.java
+++ producto.java
@@ -3,3 +3,9 @@ public class Producto{
     String nombre;
     String descripcion;
     double precio;
+    public Producto(String nombre, String descripcion, double precio){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+}
