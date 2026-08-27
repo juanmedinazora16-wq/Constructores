e6a32d3391710909000278c4f9ebfafd86662f7c
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:48:32 -0500

    Se creo la instansacion de objetos en la clase main

---
+++ main.java
@@ -0,0 +1,14 @@
+Public static void main(String[] args) {
+    Persona persona1 = new Persona("Juan" , 30, 12345);
+    Persona persona2 = new Persona("María", 25, 67890);
+    Persona persona3 = new Persona("Pedro", 40, 54321);
+
+    Auto auto1 = new Auto("Toyota", "Corolla", "Rojo");
+    Auto auto2 = new Auto("Honda", "Civic", "Azul");
+    Auto auto3 = new Auto("Ford", "Mustang", "Negro");
+
+    Producto producto1 = new Producto("Laptop", "Computadora portátil", 1200.50);
+    Producto producto2 = new Producto("Smartphone", "Teléfono inteligente", 800.00);
+    Producto producto3 = new Producto("Tablet", "Tableta electrónica", 600.00);
+    
+}
