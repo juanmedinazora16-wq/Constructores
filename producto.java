15fe2863146af77cd7bddfee22d090a2208003cf
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:14:37 -0500

    Se creo la clase producto

---
+++ producto.java
@@ -0,0 +1,11 @@
+Public class Producto{
+
+    String nombre;
+    String descripcion;
+    double precio;
+  public Producto(String nombre, String descripcion, double precio){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+}
