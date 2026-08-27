9bdad93fbb10771c4087725680dc4531707896c2
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:44:00 -0500

    Se creo el metodo constructor de la clase auto

--- auto.java
+++ auto.java
@@ -3,4 +3,9 @@ public class Auto{
     String marca;
     String modelo;
     String color;
+    public Auto(String marca, String modelo, String color){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+    }
 }
