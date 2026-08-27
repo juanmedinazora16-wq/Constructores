fff39fb54da3cd3123901d86e29a9523eac11e0f
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:12:48 -0500

    Se creo la clase persona

---
+++ persona.java
@@ -0,0 +1,11 @@
+public class Persona{
+    String nombre;
+    int edad;
+    int id;
+    public Persona(String nombre, int edad, int id){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.id = id;
+    }
+
+}
