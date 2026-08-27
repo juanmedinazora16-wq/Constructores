dc926e19081b115aca4c16df0c93a94e41114385
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:44:53 -0500

    Se creo el metodo constructor de la clase persona

--- persona.java
+++ persona.java
@@ -2,4 +2,9 @@ public class Persona{
     String nombre;
     int edad;
     int id;
+    public Persona(String nombre, int edad, int id){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.id = id;
+    }
 }
