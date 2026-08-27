ff6d2d5c81e4662969fb80d675935fdddfe63412
Juan <jmedina@uniempresarial.edu.co>
2026-08-27 13:11:40 -0500

    Se creo la clase auto

---
+++ auto.java
@@ -0,0 +1,11 @@
+Public class Auto{
+    
+    String marca;
+    String modelo;
+    String color;
+public Auto(String marca, String modelo, String color){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+}
